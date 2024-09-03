e0d1ec45b5a7ebe9dc9082027771b0e237c3889c
juanjo255 <piconcossio@gmail.com>
2024-09-03 15:22:54 -0500

    quotes for clarification

--- pneumoSchemeLoci/download_schemes_spneumoniae.sh
+++ pneumoSchemeLoci/download_schemes_spneumoniae.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -O loci https://rest.pubmlst.org/db/pubmlst_spneumoniae_seqdef/schemes/2/loci && \
-cat loci | grep -o 'https[^"]*' | sed 's/$/\/alleles_fasta/' | parallel -v -k -j 5 'curl --silent --create-dirs --output ./scheme_alleles/$(basename $(dirname {}))".fa" {}'
+cat loci | grep -o 'https[^"]*' | sed 's/$/\/alleles_fasta/' | parallel -v -k -j 5 'curl --silent --create-dirs --output "./scheme_alleles/"$(basename $(dirname {}))".fa" {}'

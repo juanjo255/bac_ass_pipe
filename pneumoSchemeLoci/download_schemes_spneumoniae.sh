984de77de6dca4b1397e96c1c4717f45fb177c14
juanjo255 <piconcossio@gmail.com>
2024-09-03 15:14:03 -0500

    now each locus will be downloaded in a different fasta for chewBACCA processing

--- pneumoSchemeLoci/download_schemes_spneumoniae.sh
+++ pneumoSchemeLoci/download_schemes_spneumoniae.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 wget -O loci https://rest.pubmlst.org/db/pubmlst_spneumoniae_seqdef/schemes/2/loci && \
-cat loci | grep -o 'https[^"]*' | sed 's/$/\/alleles_fasta/' | parallel -v -k -j 5 'mkdir $(basename $(dirname {})) && wget -q -P $(basename $(dirname {})) {}'
-cat SPN* >> alleles_spneumoniae.fasta && rm -rf SPN*
+cat loci | grep -o 'https[^"]*' | sed 's/$/\/alleles_fasta/' | parallel -v -k -j 5 'curl --create-dirs --output ./scheme_alleles/$(basename $(dirname {}))".fa" {}'

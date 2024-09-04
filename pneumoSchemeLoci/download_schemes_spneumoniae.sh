8c34179db97e7171b7d47fc2532182e712e97820
juanjo255 <piconcossio@gmail.com>
2024-09-03 20:34:12 -0500

    fixing error with loci download

--- pneumoSchemeLoci/download_schemes_spneumoniae.sh
+++ pneumoSchemeLoci/download_schemes_spneumoniae.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wd=$1
 wget -O $wd"/loci" https://rest.pubmlst.org/db/pubmlst_spneumoniae_seqdef/schemes/2/loci && \
-cat loci | grep -o 'https[^"]*' | sed 's/$/\/alleles_fasta/' | parallel -v -k -j 5 "curl --silent --create-dirs --output $wd'/scheme_alleles/'\$(basename \$(dirname {})).fa {}"
+cat $wd"/loci" | grep -o 'https[^"]*' | sed 's/$/\/alleles_fasta/' | parallel -v -k -j 5 "curl --silent --create-dirs --output $wd'/scheme_alleles/'\$(basename \$(dirname {})).fa {}"

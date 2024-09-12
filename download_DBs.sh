55d01b7b7d03f3eee993381d2ea848ff7b3a4d16
juanjo255 <piconcossio@gmail.com>
2024-09-12 12:33:17 -0500

    improving donwload of databases

--- download_DBs.sh
+++ download_DBs.sh
@@ -31,21 +31,20 @@ ariba_outDir=$wd"/ariba_out/"
 
 echo "Updating the MLST database"
 mlst-download_pub_mlst -j 5 -d $(echo $(grep -Po ".*(?=bin)" <<< $(which mlst))"db/pubmlst")
-mlst-make_blast_db
+mlst-make_blast_db && echo "done"
 
 echo "Downloading the cgMLST"
-bash $exec_path"/pneumoSchemeLoci/download_schemes_spneumoniae.sh" $path_to_scheme
+bash $exec_path"/pneumoSchemeLoci/download_schemes_spneumoniae.sh" $path_to_scheme && echo "done"
 
 echo "Downloading ARIBA db"
 mkdir -p $ariba_outDir
 ariba getref $ariba_outDir$ariba_db $ariba_outDir$ariba_db_out
-ariba prepareref -f $ariba_outDir$ariba_db_out".fa" -m $ariba_outDir$ariba_db_out".tsv" $ariba_outDir"ariba.$ariba_db.prepareref"
+ariba prepareref -f $ariba_outDir$ariba_db_out".fa" -m $ariba_outDir$ariba_db_out".tsv" $ariba_outDir"ariba.$ariba_db.prepareref" && echo "done"
 
 echo "Downloading GPSC db and metadata (10GB)"
 mkdir -p $path_to_GPSC_db
 wget -O $path_to_GPSC_db"$(basename $link_to_GPSC_db)" $link_to_GPSC_db
 wget -O $path_to_GPSC_db"$(basename $link_to_GPSC_meta)" $link_to_GPSC_meta
-
 
 
 echo "Files were downloaded in $wd"
